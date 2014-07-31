5bb54003cd41492425900d606d3a9f6e13bd07cb
Kaelan Fouwels <kfouwels@outlook.com>
2014-07-31 14:55:23 +0100

    web enabled - moved to flash

--- scraper.py
+++ scraper.py
@@ -1,47 +1,50 @@
 import json
 import urllib2
-import pprint
+#import pprint
 from bs4 import BeautifulSoup
 import sys
+from flask import Flask
+app = Flask(__name__)
 
-product = sys.argv[1]
-url = 'http://comparesupermarketprices.co.uk/supermarketItems.aspx?search='
+@app.route("/")
+def error():
+	return "no product supplied"
 
-class Item:
-	Description = u""
-	Supermarket = u""
-	Offers = u""
-	Price = u""
-	Section = u""
-	Shelf = u""
+@app.route("/<product>")
+def data(product):
 
-response = (urllib2.urlopen(url + product).read())
-response = response.replace("&nbsp", "NULL")
+	url = 'http://comparesupermarketprices.co.uk/supermarketItems.aspx?search='
 
-if isinstance(response, str):
-	print product, " Is STR"
-if isinstance(response, unicode):
-	print product, " Is unicode"
-soup = BeautifulSoup(response.decode('utf-8','ignore'))
-table = (soup.html.body.find('table', attrs={'id' : 'ctl00_contentMain_gvItems'}))
+	response = (urllib2.urlopen(url + product).read())
+	response = response.replace("&nbsp", "NULL")
 
-items = []
+	#if isinstance(response, str):
+	#	print product, " Is STR"
+	#if isinstance(response, unicode):
+	#	print product, " Is unicode"
 
-for row in table.find_all('tr'):
-	collumns = row.find_all('td')
-	info = []
-	try:
-		info = {
-			'Description' : collumns[0].string,
-			'Supermarket' : collumns[1].string,
-			'Offers' : collumns[2].string,
-			'Price' : (collumns[3].string),
-			'Section' : collumns[4].string,
-			'Shelf' : collumns[5].string,	
-		}
-		items.append(info)
-	except Exception:
-		pass;
-	
+	soup = BeautifulSoup(response.decode('utf-8','ignore'))
+	table = (soup.html.body.find('table', attrs={'id' : 'ctl00_contentMain_gvItems'}))
 
-print json.dumps(items)
+	items = []
+
+	for row in table.find_all('tr'):
+		collumns = row.find_all('td')
+		info = []
+		try:
+			info = {
+				'Description' : collumns[0].string,
+				'Supermarket' : collumns[1].string,
+				'Offers' : collumns[2].string,
+				'Price' : (collumns[3].string),
+				'Section' : collumns[4].string,
+				'Shelf' : collumns[5].string,	
+			}
+			items.append(info)
+		except Exception:
+			pass;
+	return json.dumps(items)
+
+if __name__ == "__main__":
+    app.run()
+
